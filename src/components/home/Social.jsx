28a83291bd0cc4b783f5bbc40dbfd15639c8c98b
mayeen <km.shahriar28@gmail.com>
2025-02-14 00:10:02 +0100

    Landing page implement

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://instagram.com/" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-instagram"></i>
+            </a>
+            <a href="https://dribble.com/" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-dribbble"></i>
+            </a>
+            <a href="https://github.com/mayeen" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+export default Social
