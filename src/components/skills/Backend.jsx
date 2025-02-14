fd641b42e63ff279debad684de001c8e84a9ea3b
mayeen <km.shahriar28@gmail.com>
2025-02-15 00:34:43 +0100

    skills section implement

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const Backend = () => {
+    return (
+        <div className="skills__content">
+            <h3 className="skills__title">Backend Developer</h3>
+
+            <div className="skills__box">
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <i className='bx bx-badge-check'></i>
+
+                        <div>
+                            <h3 className="skills__name">.NET</h3>
+                            <span className="skills__level">Advance</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className='bx bx-badge-check'></i>
+
+                        <div>
+                            <h3 className="skills__name">Python</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className='bx bx-badge-check'></i>
+
+                        <div>
+                            <h3 className="skills__name">Entity Framework</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <i className='bx bx-badge-check'></i>
+
+                        <div>
+                            <h3 className="skills__name">Node.js</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className='bx bx-badge-check'></i>
+
+                        <div>
+                            <h3 className="skills__name">MSSQL</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className='bx bx-badge-check'></i>
+
+                        <div>
+                            <h3 className="skills__name">RESTful Api</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Backend
